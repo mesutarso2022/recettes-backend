d647512b7913d5b758bf3d11180ad3003ef042b4
mechack tambwe <gustave@pavillonnoir.com>
2022-07-02 21:23:19 +0100

    feat: enable graphq in production

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,17 @@
 module.exports = ({ env }) => ({
-    //...
-    'import-export-entries': {
-      enabled: true,
+  //...
+  "import-export-entries": {
+    enabled: true,
+  },
+  graphql: {
+    endpoint: "/graphql",
+    shadowCRUD: true,
+    playgroundAlways: true,
+    depthLimit: 7,
+    amountLimit: 100,
+    apolloServer: {
+      tracing: false,
     },
-    //...
-  });
+  },
+  //...
+});
